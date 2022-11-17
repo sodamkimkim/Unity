bd6cbc3e5603b7f5030c3e09f8314bade1925142
sodamkimkim <theka265@gmail.com>
2022-11-17 14:04:33 +0900

    coroutine

---
+++ CSharpPractice/MainApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConstraintsOnTypeParameters
+{
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+    }
+    class RefArray<T> where T : class
+    {
+
+    }
+    class Base { }
+    class Derived : Base { }
+    class BaseArray<U> where U : Base
+    {
+
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+      
+        }
+    }
+}
