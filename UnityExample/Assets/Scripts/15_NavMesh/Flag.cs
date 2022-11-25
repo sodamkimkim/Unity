c6f38a040a35ad2dc551040157c62c4f5661b01e
sodamkimkim <theka265@gmail.com>
2022-11-25 12:25:41 +0900

    navMesh

---
+++ UnityExample/Assets/Scripts/15_NavMesh/Flag.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+}
