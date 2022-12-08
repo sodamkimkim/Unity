7c18b20b8c01c9264b417b87ace3bc4cc086824e
sodamkimkim <theka265@gmail.com>
2022-12-08 19:54:49 +0900

    callback, delegate, enum->factory-pattern

--- UnityExample/Assets/Scripts/inheritate/RentShop.cs
+++ UnityExample/Assets/Scripts/inheritate/RentShop.cs
@@ -1,10 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RentShop : MonoBehaviour
 {
     public Menu menu = null;
     public Player222 player = null;
-
+    private void Start()
+    {
+        //menu.SetOnClickCallback((Menu.EType)0, OnClickCar);
+        //menu.SetOnClickCallback((Menu.EType)1, OnClickBoat);
+        //menu.SetOnClickCallback(Menu.EType.Bike, OnClickBike);
+        for (int i = 0; i < (int)Menu.EType.Len; ++i)
+            menu.SetOnClickCallback((Menu.EType)i, OnClickCallback);
+    }
+    //public void OnClickCar()
+    //{
+    //    Debug.Log("Ride to Car");
+    //    player.RideTo(new Car());
+    //}
+    //public void OnClickBoat()
+    //{
+    //    Debug.Log("Ride to Boat");
+    //    player.RideTo(new Boat());
+    //}
+    //public void OnClickBike()
+    //{
+    //    Debug.Log("Ride to Bike");
+    //    player.RideTo(VehicleFactory(Menu.EType.Bike));
+    //}
+    public void OnClickCallback(Menu.EType _type)
+    {
+        Debug.Log("Ride to " + _type.ToString());
+        player.RideTo(VehicleFactory(_type));
+    }
+    // factory pattern
+    private Vehicle VehicleFactory(Menu.EType _type)
+    {
+        switch (_type)
+        {
+            case Menu.EType.Bike:
+                return new Bike();
+            case Menu.EType.Car:
+                return new Car();
+            case Menu.EType.Boat:
+                return new Boat();
+        }
+        return null;
+    }
 }
