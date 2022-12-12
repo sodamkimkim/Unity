2e0430683d33dcd76e9bc4fea9711cbbbd7ac23d
sodamkimkim <theka265@gmail.com>
2022-12-12 14:56:59 +0900

    space invader

--- UnityExample/Assets/Scripts/19_SpaceInvaders/Invader.cs
+++ UnityExample/Assets/Scripts/19_SpaceInvaders/Invader.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Invader : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D _collision)
+{    private void OnTriggerEnter2D(Collider2D _collision)
     {
         if(_collision.CompareTag("SI_Missile"))
         {
-            Debug.Log("dd");
             Destroy(_collision.gameObject);
             Destroy(gameObject);
         }
     }
+
 }
