e4cba827fb88c3045179b612f25968975c93c9dc
sodamkimkim <theka265@gmail.com>
2022-12-26 18:06:33 +0900

    2D &3D effect

--- UnityExample/Assets/Scripts/23_Terrain/Tourlist.cs
+++ UnityExample/Assets/Scripts/23_Terrain/Tourlist.cs
@@ -5,34 +5,53 @@ using UnityEngine;
 // 과제 ) 각도 못넘어가게 
 public class Tourlist : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject fxPangPrefab = null;
+
+    [SerializeField]
+    private GameObject fxFireBoost = null;
+
     private CharacterController cc = null;
     private Camera cam = null;
     private float moveSpeed = 1000f;
-    private float rotSpeed = 100f;
+    private float rotSpeed = 50f;
     private Vector3 camRot = Vector3.zero;
+    Vector3 initialCamRot = Vector3.zero;
 
     private void Awake()
     {
         cc = GetComponent<CharacterController>();
         cam = GetComponentInChildren<Camera>();
+        //camRot = cam.transform.rotation.eulerAngles;
+        initialCamRot = cam.transform.rotation.eulerAngles;
     }
+
     private void Update()
     {
         MovingProcess();
         LookProcess();
+        if (Input.GetMouseButtonDown(0))
+            SpawnFxPang();
     }
     private void MovingProcess()
     {
         float axisH = Input.GetAxis("Horizontal");
         float axisV = Input.GetAxis("Vertical");
-
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        GameObject bostGo = null;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             moveSpeed = 5000f;
+            Vector3 pos = transform.position;
+            pos.y = transform.position.y - transform.localScale.y + 1f;
+
+            bostGo = Instantiate(fxFireBoost, pos, Quaternion.Euler(0f, 0f, 0f));
+            bostGo.transform.SetParent(transform);
         }
-        if(Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             moveSpeed = 1000f;
+            Destroy(bostGo);
+
         }
 
         Vector3 camF = cam.transform.forward;
@@ -56,14 +75,42 @@ public class Tourlist : MonoBehaviour
     }
     private void LookProcess()
     {
+
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        camRot.x += -mouseY * rotSpeed * Time.deltaTime;
-        camRot.y += mouseX * rotSpeed * Time.deltaTime;
         camRot.z = 0f;
 
-        cam.transform.rotation = Quaternion.Euler(camRot);
+        Debug.Log("Mouse X : " + mouseX);
+        Debug.Log("Mouse Y : " + mouseY);
+        camRot.x = -mouseY * rotSpeed * Time.deltaTime;
+        camRot.y = mouseX * rotSpeed * Time.deltaTime;
+        if (camRot.x >= 5f)
+            camRot.x = 5f;
+        if (camRot.x <= -5f)
+            camRot.x = -5f;
+
+        if (camRot.y >= 15f)
+            camRot.y = 15f;
+        if (camRot.y <= -15f)
+            camRot.y = -15f;
+
+        Vector3 objQ = transform.rotation.eulerAngles;
+        Vector3 camQ = cam.transform.rotation.eulerAngles;
+
+        if (Mathf.Abs(initialCamRot.x - camQ.x) <=10f  && Mathf.Abs(initialCamRot.y - camQ.y) <= 10f)
+            cam.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + camRot);
+       
+
+
+
+
+
+
     }
-    
+    private void SpawnFxPang()
+    {
+        Instantiate(fxPangPrefab, cam.transform.position + (cam.transform.forward * 3f), Quaternion.identity);
+    }
+
 }
