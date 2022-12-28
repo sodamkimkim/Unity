7e8106155df5086ab92fccd070640f725ab00114
sodamkimkim <theka265@gmail.com>
2022-12-28 19:28:37 +0900

    메뉴판 포지션 이동 IsAnimation == true일 때도 상태 변경되게 수정

--- UnityExample/Assets/Scripts/24_UGUI/UIMenu.cs
+++ UnityExample/Assets/Scripts/24_UGUI/UIMenu.cs
@@ -7,7 +7,7 @@ public class UIMenu : MonoBehaviour
     private Transform tr = null;
     private RectTransform rt = null;
     private bool isAnimation = false;
-    private float movingSpeed = 5f;
+    private float movingSpeed = 0.1f;
     private bool isShow = true;
 
     private void Awake()
@@ -17,10 +17,14 @@ public class UIMenu : MonoBehaviour
     }
     private void Update()
     {
-        if (isAnimation == false && Input.GetKeyDown(KeyCode.Space))
+        //if (isAnimation == false && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             isShow = !isShow;
-            if (isShow) Show();
+            if (isShow)
+            {
+                Show();
+            }
             else Hide();
         }
     }
@@ -40,10 +44,11 @@ public class UIMenu : MonoBehaviour
 
         while (t < 1f)
         {
+            IsAnimationDestChanging(ref _startX, ref _endX);
             // # Lerp(Linear Intepolation)
             // 밑의 방식대로 newX값을 보간하면(tr.position.x 사용) 시작과 끝점과의 거리가 점점 줄어드니까
             // tr.position.x -> endX까지 가는 속도가 점점 빨라진다.
-            float newX = Mathf.Lerp(tr.position.x, _endX, Time.deltaTime * movingSpeed);          
+            float newX = Mathf.Lerp(tr.position.x, _endX, Time.deltaTime * movingSpeed);
             //float newX = Mathf.Lerp(_startX, _endX, Time.deltaTime * movingSpeed);
             newPos = rt.position;
             newPos.x = newX;
@@ -52,10 +57,30 @@ public class UIMenu : MonoBehaviour
             t += Time.deltaTime * movingSpeed;
             yield return null;
         }
-
+        IsAnimationDestChanging(ref _startX, ref _endX);
         newPos = rt.position;
         newPos.x = _endX;
         rt.position = newPos;
         isAnimation = false;
     }
-}
+    private void IsAnimationDestChanging(ref float _startX, ref float _endX)
+    {
+        if (isAnimation == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            // Show()하고 있던 경우 -> 그 자리에서 Hide()
+            if (isShow)
+            {
+                _startX = tr.position.x;
+                _endX = -rt.sizeDelta.x;
+
+                // Hide()하고 있던 경우 -> 그 자리에서 Show()
+            }
+            else
+            {
+                _startX = tr.position.x;
+                _endX = 0f;
+            }
+
+        }
+    }
+} // end of class
